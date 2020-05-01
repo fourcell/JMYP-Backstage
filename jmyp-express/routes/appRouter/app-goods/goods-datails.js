bf9cf6ee53b092877ab577a07dac763f4e329b0d
isv_吕进浩 <hz936302257@163.com>
2020-05-01 15:30:16 +0800

    feat:"参数名字更改"

--- jmyp-express/routes/appRouter/app-goods/goods-datails.js
+++ jmyp-express/routes/appRouter/app-goods/goods-datails.js
@@ -6,13 +6,13 @@ const mysql = require('../../../api/mysql/index');
 /* GET home page. */
 router.get('/', async function (req, res, next) {
     res.append('Access-Control-Allow-Origin', '*');
-    const type = req.query.type   //接受post参数
+    const id = req.query.id   //接受post参数
 
     const data = {}   //返回的data数据
     let sku = []    //sku
     let param = {}
 
-    let select = await mysql(`SELECT * FROM product,sku_product WHERE product.procuct_id = 1 AND sku_product.product_id = ${type} `)
+    let select = await mysql(`SELECT * FROM product,sku_product WHERE product.procuct_id = 1 AND sku_product.product_id = ${id} `)
     //    console.log(JSON.parse( select[0].sp_data).sex)
     sku = select.map(item => {
         return {
@@ -20,8 +20,8 @@ router.get('/', async function (req, res, next) {
             img: item.picture,  //sku图片
             size: JSON.parse(item.sp_data).size,   //sku商品的尺码
             color: JSON.parse(item.sp_data).color,    //sku商品颜色
-            price: item.price,   //原价
-            vip_price: item.price,//降价之后
+            price: item.price,   //sku显示的价格，原价
+            vip_price: item.price,//sku显示的价格，就是实际支付的价格
         }
     })
 
@@ -31,8 +31,8 @@ router.get('/', async function (req, res, next) {
         sales: select[0].sales,   //销量
         describes: select[0].describes,   //销量
         is_put: select[0].is_put, //评论
-        price: select[0].price, //评论
-        vip_price: select[0].vip_price //评论
+        price: select[0].price, //商品的原价
+        vip_price: select[0].vip_price //商品降价之后的价格
     }
 
     try {
